cbeea29c495f830ee3906ba51299fe9bd554d1f8
Richard <rprice@greenpeace.org>
2022-10-28 11:16:19 +0200

    Added local handler function for missing env var - will remove later

--- system/getsecret.py
+++ system/getsecret.py
@@ -1,8 +1,16 @@
 # Install Google Libraries
 from google.cloud import secretmanager
+import os
+
+
+def check_environ_and_set_vars(path: str = "/Users/rich/greenpeace_workspace/gcp_key/techlab-coding-team-e231abddafaf.json"):
+    if "GOOGLE_APPLICATION_CREDENTIALS" not in list(os.environ.keys()):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
 
 # Setup the Secret manager Client
+check_environ_and_set_vars()
 client = secretmanager.SecretManagerServiceClient()
+
 
 def getsecrets(secret_name, project_id):
     resource_name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
